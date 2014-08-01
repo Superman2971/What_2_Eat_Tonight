77111d3b87e2fb732364fe240108d1a851a32cba
Superman2971 <goldfarb.ian@gmail.com>
2014-08-01 14:33:20 -0700

    Fresh start for my project, I have html and css ready to be implemeneted into this new rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+What2EatTonight::Application.config.secret_key_base = '26622132218b376728b97b38b1444a39489f3e8515bf806ece00df777ac87da0c7196c4994eaabc9852689b7bb1af7f43e83e4e47ac94c4289e66d2e41b56a10'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+What2EatTonight::Application.config.session_store :cookie_store, key: '_what_2_eat_tonight_session'
