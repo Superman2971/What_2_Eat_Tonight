f62917a6cdd434730136d861d99bedadf4c37601
Superman2971 <goldfarb.ian@gmail.com>
2014-08-07 17:06:27 -0700

    FUNCTIONAL VERSION, includes auto login after sign up, my auth login is complete-ish

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < ApplicationController
   def new
     #present an empty login form
     @user = User.new
-    @is_login = true
   end
 
   def create
@@ -10,16 +9,10 @@ class SessionsController < ApplicationController
     u = User.where(email: params[:user][:email]).first
     #prove that they are indeed who they say they are (password)
     if u && u.authenticate(params[:user][:password])
-
-      if u.is_active == false
-        redirect_to reactivate_user_path(u.id)
-      else
-
       #store as a cookie in browser, their id
       #indicating that they are logged in
       session[:user_id] = u.id.to_s
       redirect_to root_path
-      end
     else
       #go back to the login page
       redirect_to new_session_path
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   def create
     user = User.new(params.require(:user).permit(:name, :email, :password, :password_confirmation))
     if user.save
+      u = User.where(email: params[:user][:email]).first
+      session[:user_id] = u.id.to_s
       redirect_to root_path
     else
       redirect_to new_user_path
