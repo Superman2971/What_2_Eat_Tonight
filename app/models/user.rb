365166c135bc5741c5732839800b57cd293c640c
Superman2971 <goldfarb.ian@gmail.com>
2014-08-08 16:34:42 -0700

    precompile for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,13 @@ class User
 
   has_secure_password
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true, 
+                    format: { with: VALID_EMAIL_REGEX }, 
+                    uniqueness: { case_sensitive: false }
+  
+  validates :password, length: { minimum: 6 }
+
   has_many :comments
 end
