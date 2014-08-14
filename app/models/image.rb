b7b271f5bbf156b0aab12fc21d99e0fc1df790bf
Superman2971 <goldfarb.ian@gmail.com>
2014-08-14 04:23:36 -0700

    Added navigation to the carousel

--- app/models/image.rb
+++ app/models/image.rb
@@ -7,6 +7,12 @@ class Image
   belongs_to :user
 
   has_mongoid_attached_file :image
+  
+  validates_presence_of :image
   validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+  validates_presence_of :restaurant
+  validates_presence_of :location
+
 
 end
