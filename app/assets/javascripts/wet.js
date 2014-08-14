82de6a03dd4b37834cd7f61a13cda1e61e80c854
Superman2971 <goldfarb.ian@gmail.com>
2014-08-13 23:04:54 -0700

    FUNCTIONAL, includes randomized BG images, upload image feature with css, logo, and connected images uploaded to user model

--- app/assets/javascripts/wet.js
+++ app/assets/javascripts/wet.js
@@ -1,5 +1,9 @@
 var wetApp = angular.module("wetApp",[]);
 
 wetApp.controller("wetController",function($scope){
+
+  $scope.modal = function(restaurant, location, user){
+    alert(restaurant + "\n" + location + "\n" + "Contributed by: " + user);
+  }
   
 });
