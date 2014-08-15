7e09fc532f00b47d4e0747cde44266f54a618f69
Superman2971 <goldfarb.ian@gmail.com>
2014-08-15 14:45:37 -0700

    Fixed the Google Map loading issue

--- app/assets/javascripts/wet.js
+++ app/assets/javascripts/wet.js
@@ -8,38 +8,40 @@ wetApp.controller("wetController",function($scope){
     $scope.location = location;
     $scope.user = user;
 
-    $scope.codeAddress = function() {
-      var address = document.getElementById("address").value;
-      geocoder.geocode( { 'address': address}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          map.setCenter(results[0].geometry.location);
-          var marker = new google.maps.Marker({
-              map: map,
-              animation: google.maps.Animation.DROP,
-              position: results[0].geometry.location
-          });
-        } else {
-          alert("Geocode was not successful for the following reason: " + status);
-        }
-      });
-    }
-
+    var address = $scope.location;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            animation: google.maps.Animation.DROP,
+            position: results[0].geometry.location
+        });
+        setTimeout(function() {
+            google.maps.event.trigger(map,'resize');
+        }, 500);
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
   }
 
-    // Google Maps Script
-    var geocoder;
-    var map;
-    function initialize() {
-      geocoder = new google.maps.Geocoder();
-      var latlng = new google.maps.LatLng(34.013, -118.494);
-      var mapOptions = {
-        zoom: 15,
-        center: latlng
-      }
-      map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-    }
 
-    google.maps.event.addDomListener(window, 'load', initialize);
+
+  // Google Maps Script
+  var geocoder;
+  var map;
+  function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(34.013, -118.494);
+    var mapOptions = {
+      zoom: 15,
+      center: latlng
+    }
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
 
 
 });
